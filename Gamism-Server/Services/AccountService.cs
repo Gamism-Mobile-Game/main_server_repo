c2b2a21e037484d914a5c97a03d9f1caee5d3913
hyuli0520 <minhyeogjeon945@gmail.com>
2025-09-04 11:15:44 +0900

    feat: init setting

---
+++ Gamism-Server/Services/AccountService.cs
@@ -0,0 +1,28 @@
+﻿using GameDB;
+
+namespace Gamism_Server.Services
+{
+    public class AccountService
+    {
+        AccountDbContext _dbContext;
+        
+        public AccountService(AccountDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<LoginAccountPacketRes> LoginGoogleAccount(string token)
+        {
+            LoginAccountPacketRes res = new LoginAccountPacketRes();
+
+            return res;
+        }
+
+        public async Task<LoginAccountPacketRes> LoginGuestAccount(string userId)
+        {
+            LoginAccountPacketRes res = new LoginAccountPacketRes();
+
+            return res;
+        }
+    }
+}
